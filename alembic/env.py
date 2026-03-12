523b99379cb6b406275c4d37ce5141b1f99c3829
Mahesh <maheshkumaree25@gmail.com>
2026-03-12 22:30:21 +0530

    refactor: use standard fileConfig in alembic/env.py

--- alembic/env.py
+++ alembic/env.py
@@ -1,4 +1,4 @@
-from logging.config import file_config
+from logging.config import fileConfig
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 from alembic import context
@@ -12,7 +12,7 @@ config = context.config
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
-    file_config(config.config_file_name)
+    fileConfig(config.config_file_name)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
